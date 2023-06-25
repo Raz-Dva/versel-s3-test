ca1842ad9f5429afc643789c5ff28cfe932003be
Honcharov_Ivan <my.poshta.goncharov@gmail.com>
2023-06-25 19:18:29 +0200

    router

--- router/router.js
+++ router/router.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
 
 router.get('/second', (req, res) => {
     res.sendFile(`${process.cwd()}/public/second.html`)
